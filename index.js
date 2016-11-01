978295848ed25d778bb32575b57ca378fcdfbebc
beverly <beverlybribiesca07@gmail.com>
2016-11-01 18:00:54 +0000

    new message

---
+++ index.js
@@ -0,0 +1,39 @@
+// write your code below!
+
+var basicTeenager = function(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else {
+    return undefined
+  }
+}
+var teenager = function(age)      {
+  if (age >= 13 && age <=19) {
+    return "You are a teenager!";
+} else {
+  return "You are not a teenager";
+  }
+}
+
+var ageChecker = function(age) {
+if (age >= 13 && age <= 19) {
+  return "You are a teenager!";
+}  else {
+  return "You are a teenager!";
+   }
+}
+
+// check to see if age is between 13 and 19. If it is, return, you are a teenager. If it isn't, return undefined.
+function ternaryTeenager = funtion (age) {
+if (age >= 12 ) {
+  return "You are a kid!";
+} else {
+  return "You are a kid!";
+}
+
+
+}
+
+function switchAge(age) {
+
+}
